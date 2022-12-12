71fecd72d21479295753260bb4964f222309ecbc
Anton Pryakhin <dr.pryah@gmail.com>
2022-12-12 20:34:24 +0300

    add example of Event Loop

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import colors from "colors";
 console.log(colors.red("Hello World!"));
+
+console.log("Record 1");
+Promise.resolve().then(() => console.log("Record 2"));
+setTimeout(() => {
+  console.log("Record 3");
+  Promise.resolve().then(() => {
+    console.log("Record 4");
+  });
+});
+Promise.resolve().then(() => {
+  Promise.resolve().then(() => {
+    console.log("Record 5");
+  });
+  console.log("Record 6");
+});
+console.log("Record 7");
