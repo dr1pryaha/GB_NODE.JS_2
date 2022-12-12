34ee4d080e4f6d9fc2b5fe2175c227c2e92aa49c
Anton Pryakhin <dr.pryah@gmail.com>
2022-12-12 22:30:54 +0300

    add emitter.on

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 import colors from "colors";
+import EventEmitter from "events";
+
+//Задание № 1
+
 console.log(colors.red("Hello World!"));
 
 console.log("Record 1");
-Promise.resolve().then(() => console.log("Record 2"));
 setTimeout(() => {
-  console.log("Record 3");
+  console.log("Record 2");
   Promise.resolve().then(() => {
-    console.log("Record 4");
+    setTimeout(() => {
+      console.log("Record 3");
+      Promise.resolve().then(() => {
+        console.log("Record 4");
+      });
+    });
   });
 });
-Promise.resolve().then(() => {
-  Promise.resolve().then(() => {
-    console.log("Record 5");
-  });
-  console.log("Record 6");
+console.log("Record 5");
+Promise.resolve().then(() =>
+  Promise.resolve().then(() => console.log("Record 6"))
+);
+
+//Задание № 2
+
+class TimerEmitter extends EventEmitter {}
+const emitter = new TimerEmitter();
+
+emitter.on("timerTick", ([dateInFuture, timer]) => {
+  const dateNow = new Date();
+  if (dateNow >= dateInFuture) {
+    emitter.emit("timerEnd", timer);
+  } else {
+    console.log(getPrettyTime((dateInFuture - dateNow) / 1000), " left");
+  }
 });
-console.log("Record 7");
+
+emitter.on("timerEnd", timer => {
+  console.log("Time is Up!");
+  clearInterval(timer);
+});
+
+const getPrettyTime = seconds => {
+  const arr = [
+    Math.floor(seconds % 60),
+    Math.floor((seconds / 60) % 60),
+    Math.floor(seconds / (60 * 60)) % 24,
+    Math.floor(seconds % 60),
+  ];
+};
+
+// //регистрация события
+// emitterObject.on("send", Handler.send);
+
+// //генерация события
+// generateNewCustomer().then(customer =>
+//   emitterObject.emit(customer.type, customer.payload)
+// );
+
+// //обработка ошибки ОБЯЗАТЕЛЬНА!!!
+// emitterObject.emit("error", new Error("Что-то пошло не так!"));
